ac8db8e122f952486a5deb97e55994d78472bdec
apollobing <taras.career@gmail.com>
2024-03-14 19:41:55 +0200

    2. Связь слоев через Spring DI. [#504856]

--- src/main/java/ru/job4j/dreamjob/repository/MemoryCandidateRepository.java
+++ src/main/java/ru/job4j/dreamjob/repository/MemoryCandidateRepository.java
@@ -12,8 +12,6 @@ import java.util.Optional;
 @Repository
 public class MemoryCandidateRepository implements CandidateRepository {
 
-    private static final MemoryCandidateRepository INSTANCE = new MemoryCandidateRepository();
-
     private int nextId = 1;
 
     private final Map<Integer, Candidate> candidates = new HashMap<>();
@@ -25,10 +23,6 @@ public class MemoryCandidateRepository implements CandidateRepository {
         save(new Candidate(0, "Liza", "Middle Java Developer", LocalDateTime.now()));
         save(new Candidate(0, "Tom", "Middle+ Java Developer", LocalDateTime.now()));
         save(new Candidate(0, "Samantha", "Senior Java Developer", LocalDateTime.now()));
-    }
-
-    public static MemoryCandidateRepository getInstance() {
-        return INSTANCE;
     }
 
     @Override
