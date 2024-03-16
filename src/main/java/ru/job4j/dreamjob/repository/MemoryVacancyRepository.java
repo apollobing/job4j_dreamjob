388e7c1e81385dfb251ed8bf678bcaf60f3c3d99
apollobing <taras.career@gmail.com>
2024-03-16 20:45:48 +0200

    2. Формы. Списки. [#504854]

--- src/main/java/ru/job4j/dreamjob/repository/MemoryVacancyRepository.java
+++ src/main/java/ru/job4j/dreamjob/repository/MemoryVacancyRepository.java
@@ -20,12 +20,12 @@ public class MemoryVacancyRepository implements VacancyRepository {
     private final Map<Integer, Vacancy> vacancies = new ConcurrentHashMap<>();
 
     private MemoryVacancyRepository() {
-        save(new Vacancy(0, "Intern Java Developer", "Supply department", LocalDateTime.now(), true));
-        save(new Vacancy(0, "Junior Java Developer", "Finance department", LocalDateTime.now(), false));
-        save(new Vacancy(0, "Junior+ Java Developer", "IT department", LocalDateTime.now(), true));
-        save(new Vacancy(0, "Middle Java Developer", "HR department", LocalDateTime.now(), false));
-        save(new Vacancy(0, "Middle+ Java Developer", "Security department", LocalDateTime.now(), false));
-        save(new Vacancy(0, "Senior Java Developer", "Head department", LocalDateTime.now(), true));
+        save(new Vacancy(0, "Intern Java Developer", "Supply department", LocalDateTime.now(), true, 1));
+        save(new Vacancy(0, "Junior Java Developer", "Finance department", LocalDateTime.now(), false, 2));
+        save(new Vacancy(0, "Junior+ Java Developer", "IT department", LocalDateTime.now(), true, 3));
+        save(new Vacancy(0, "Middle Java Developer", "HR department", LocalDateTime.now(), false, 3));
+        save(new Vacancy(0, "Middle+ Java Developer", "Security department", LocalDateTime.now(), false, 2));
+        save(new Vacancy(0, "Senior Java Developer", "Head department", LocalDateTime.now(), true, 1));
     }
 
     @Override
@@ -45,7 +45,7 @@ public class MemoryVacancyRepository implements VacancyRepository {
         return vacancies.computeIfPresent(vacancy.getId(),
                 (id, oldVacancy) -> new Vacancy(oldVacancy.getId(), vacancy.getTitle(),
                         vacancy.getDescription(), vacancy.getCreationDate(),
-                        vacancy.getVisible())) != null;
+                        vacancy.getVisible(), vacancy.getCityId())) != null;
     }
 
     @Override
